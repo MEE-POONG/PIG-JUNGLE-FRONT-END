65be2f541906618fadd924f22197b247593f85b3
Ploy <117616170+yaminP@users.noreply.github.com>
2022-11-09 14:00:28 +0700

    หน้าสินค้า

---
+++ pages/Productpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../components/Product/StylePage.css'
+
+export default function Productpage() {
+    return(
+        <div>Welcome to my Product page</div>
+    )
+    
+}
+ 
--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-aa
+<Product/>
       </div>
     </>
   );
