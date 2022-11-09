34d12ea210051ca977b0470554da9d833756fee1
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2022-11-09 12:07:19 +0700

    benz

--- components/Navbars/IndexNavbar.jsx
+++ components/Navbars/IndexNavbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import style from './IndexNavbar.module.css';
+
 // components
 import { Container, Navbar, Nav, NavDropdown, Offcanvas, Form, FormControl, Button, Image, Row, Col } from 'react-bootstrap';
 
@@ -11,11 +13,11 @@ export default function IndexNavbar() {
       <Navbar className="header-area header-sticky wow slideInDown" expand="lg">
         <Container >
           <Navbar.Brand href="/">
-            <Image src={'images/iodesign.png'} alt="logo-nav" />
+            <Image src={'images/logo.png'} alt="logo-nav" className="" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav className="main-nav">
+            <Nav className={style.mainNav}>
               <Nav.Link href="/" className="font-bold">Home</Nav.Link>
               <Nav.Link href="/gallery" className="font-bold">gallery</Nav.Link>
               <Nav.Link href="/catalog" className="font-bold">catalog</Nav.Link>
