4d344f1665f9fedec427a1761d4cd2738049da52
Carter <114277619+Carter44243@users.noreply.github.com>
2022-11-09 16:33:25 +0700

    nim

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,7 @@ import PageChange from "../components/PageChange/PageChange.js";
 import SSRProvider from "react-bootstrap/SSRProvider";
 import "assets/scss/style.scss";
 import "assets/scss/style.css";
+import "assets/scss/aboutstyle.css";
 
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 // import "../styles/tailwind.scss";
