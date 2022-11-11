11d48288c6be2f6f1d1fceeaa0bd494fe0350c4c
Primpunyaporn <117614438+Primpunyaporn@users.noreply.github.com>
2022-11-11 10:13:21 +0700

    eieieiei

--- components/layouts/IndexLayout.jsx
+++ components/layouts/IndexLayout.jsx
@@ -1,7 +1,7 @@
 import IndexNavbar from "components/Navbars/IndexNavbar";
 import About from "../About/About";
 
-import Footer from "../Footers/Footer";
+
 
 import Footer from "../Footers/Footer";
 import Loginpage from "../Login/Login";
