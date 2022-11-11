f25e0d81618832a1d45bef2eeece9feb45c3b6df
mind2421 <thutiya.ch@gmail.com>
2022-11-11 14:46:39 +0700

    fix-register-login-mind

---
+++ pages/login.jsx
@@ -0,0 +1,13 @@
+import Loginpage from "@/components/Login/Login";
+import Registerpage from "@/components/Register/Register";
+import React from "react";
+
+export default function login(){
+    return(
+        <>
+
+        <Loginpage/>
+        
+        </>
+    );
+}
