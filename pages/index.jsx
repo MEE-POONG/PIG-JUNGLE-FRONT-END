e818e1136ebac13880273d9866cfdd8f98cf09d6
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2022-11-11 17:58:47 +0700

    index update

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import IndexLayout from "components/layouts/IndexLayout";
 import { useRouter } from 'next/router';
 import ImageSider from '@/components/ImageSlider/ImageSlider';
 import Product_ShowIndex from '@/components/Product_ShowIndex/Product_ShowIndex';
+import Product_Test from '@/components/Product_Test/Product_Test';
 // import IndexNavbar from '@/components/Navbars/IndexNavbar';
 
 export default function HomePage() {
@@ -20,6 +21,7 @@ export default function HomePage() {
       <div id='home-page' name="home-page" className='home-page'>
         <ImageSider/>
         <Product_ShowIndex/>
+        <Product_Test/>
       </div>
     </>
   );
