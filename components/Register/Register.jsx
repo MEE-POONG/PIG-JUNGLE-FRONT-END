a367969de0334914e30e705764b7082c43420012
mind2421 <thutiya.ch@gmail.com>
2022-11-12 12:17:03 +0700

    fix-lg-rg-12-11-2022

--- components/Register/Register.jsx
+++ components/Register/Register.jsx
@@ -1,30 +1,31 @@
 import React from "react"
 import { Container, Navbar, Nav, NavDropdown, Offcanvas, Form, FormControl, Button, Image, Row, Col, NavbarBrand } from 'react-bootstrap';
 
-export default function Registerpage (){
-    return(
-    <>
-<div className="box-Container-Rg">
-<div className='Container-Rg'>
-    <h2 className='title-Lg-Rg'>Register</h2>
-    <form>
-    
-    <input className='input-Lg-Rg' type="text" id="UserName" name="UserName" placeholder="UserName"/>
-    
-    <input className='input-Lg-Rg' type="text" id="Email" name="Email" placeholder="Email" />
-    
-    <input className='input-Lg-Rg' type="text" id="Password" name="Password" placeholder="Password"  />
-    
-    <input className='input-Lg-Rg' type="text" id="ConfirmPassword" name="ConfirmPassword" placeholder="ConfirmPassword" />
-    </form>
-    <div className="box-btu-Lg-Rg">
-    <a className="btu-Lg-Rg" type='sign-up'  href="/address">SIGN UP</a>
-    </div>
-    <div className='box-text'>
-        <a className='text-Lg-Rg' href="/login"> Login </a>
-    </div>
-</div>
-</div>
-</>
-)}
+export default function Register() {
+    return (
+        <>
+            <div className="box-Container-Rg">
+                <div className='Container-Rg'>
+                    <h2 className='title-Lg-Rg'>Sign up</h2>
+                    <form>
+
+                        <input className='input-Lg-Rg' type="text" id="UserName" name="UserName" placeholder="UserName" />
+
+                        <input className='input-Lg-Rg' type="text" id="Email" name="Email" placeholder="Email" />
+
+                        <input className='input-Lg-Rg' type="text" id="Password" name="Password" placeholder="Password" />
+
+                        <input className='input-Lg-Rg' type="text" id="ConfirmPassword" name="ConfirmPassword" placeholder="ConfirmPassword" />
+                    </form>
+                    <div className="box-btu-Lg-Rg">
+                        <a className="btu-Lg-Rg" type='sign-up' href="/address">SIGN UP</a>
+                    </div>
+                    <div className='box-text'>
+                        <a className='text-Lg-Rg' href="/login"> Sign in </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
 
