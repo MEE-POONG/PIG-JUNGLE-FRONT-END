9320b2dc9fdc2e5bcbf47bb8a6f5b5ddfbf5f1db
devilzeros <devilzeros@outlook.co.th>
2022-11-12 22:49:10 +0700

    est

---
+++ components/Bg/BackGroundMain.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+// components
+
+
+export default function BackGroundMain() {
+    return (
+        <>
+            <div className="star s-1 four-pointed-star"></div>
+            <div className="star s-2 four-pointed-star"></div>
+            <div className="star s-3 four-pointed-star"></div>
+            <div className="star s-4 four-pointed-star"></div>
+            <div className="star s-5 four-pointed-star"></div>
+            <div className="star s-6 four-pointed-star"></div>
+            <div className="star s-7 four-pointed-star"></div>
+        </>
+    );
+}
--- components/layouts/IndexLayout.jsx
+++ components/layouts/IndexLayout.jsx
@@ -1,12 +1,14 @@
 import IndexNavbar from "components/Navbars/IndexNavbar";
+import BackGroundMain from "../Bg/BackGroundMain";
 import Footer from "../Footers/Footer";
 
 export default function IndexLayout({ children }) {
   return (
     <>
+      <BackGroundMain />
       <IndexNavbar />
-        {children}
-        {/* <Footer/> */}
+      {children}
+      {/* <Footer/> */}
     </>
 
 
