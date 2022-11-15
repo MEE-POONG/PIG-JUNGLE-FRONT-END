d0cca55ff5ba910e10cbfe0d826d4398508e678c
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2022-11-15 17:00:43 +0700

    Product

--- pages/product.jsx
+++ pages/product.jsx
@@ -2,6 +2,7 @@ import ProductShow from "container/Product/Productshow";
 import React from "react";
 import Head from 'next/head';
 import IndexLayout from "components/layouts/IndexLayout";
+import ProductList from "container/Product/ProductList";
 
 
 export default function ProductPage() {
@@ -16,7 +17,8 @@ export default function ProductPage() {
           <link rel="icon" href="/images/logo.png" />
         </Head>
         <div id='product-page' name="product-page" className='product-page'>
-          <ProductShow/>
+          {/* <ProductShow/> */}
+          <ProductList/>
         </div>
       </>
     )
