1245258d29b908149c1f6ecf144c3323e3702a45
devilzeros <devilzeros@outlook.co.th>
2022-11-22 10:50:41 +0700

    est

--- components/Navbars/IndexNavbar.jsx
+++ components/Navbars/IndexNavbar.jsx
@@ -44,15 +44,15 @@ export default function IndexNavbar() {
           <Navbar.Collapse id="navbarScroll">
             <Nav className="d-flex align-items-center w-100 text-center">
               <div className="d-lg-flex w-40 justify-content-center">
-                <Nav.Link href="/" className="font-menu font-bold mx-2">
+                <Nav.Link href="/" className="font-menu font-bold mx-auto">
                   <div className="active">หน้าแรก</div>
                   <div className="n-active">หน้าแรก</div>
                 </Nav.Link>
-                <Nav.Link href="/product" className="font-menu font-bold mx-2">
+                <Nav.Link href="/product" className="font-menu font-bold mx-auto">
                   <div className="active">ร้านค้า</div>
                   <div className="n-active">ร้านค้า</div>
                 </Nav.Link>
-                <Nav.Link href="/about" className="font-menu font-bold mx-2">
+                <Nav.Link href="/about" className="font-menu font-bold mx-auto">
                   <div className="active">เกี่ยวกับเรา</div>
                   <div className="n-active">เกี่ยวกับเรา</div>
                 </Nav.Link>
@@ -61,15 +61,15 @@ export default function IndexNavbar() {
                 <Image src={'images/logo.png'} alt="logo-nav" width={navLogo} style={{ transition: "all 0.5s" }} />
               </Navbar.Brand>
               <div className="d-lg-flex w-40 justify-content-center">
-                <Nav.Link href="/Contact" className="font-menu font-bold mx-2">
+                <Nav.Link href="/Contact" className="font-menu font-bold mx-auto">
                   <div className="active">ติดต่อ</div>
                   <div className="n-active">ติดต่อ</div>
                 </Nav.Link>
-                <Nav.Link href="/sign_in" className="font-menu font-bold mx-2">
+                <Nav.Link href="/sign_in" className="font-menu font-bold mx-auto">
                   <div className="active">เข้าสู่ระบบ</div>
                   <div className="n-active">เข้าสู่ระบบ</div>
                 </Nav.Link>
-                <Nav.Link href="/sign_up" className="font-menu font-bold mx-2">
+                <Nav.Link href="/sign_up" className="font-menu font-bold mx-auto">
                   <div className="active">สมัครสมาชิก</div>
                   <div className="n-active">สมัครสมาชิก</div>
                 </Nav.Link>
