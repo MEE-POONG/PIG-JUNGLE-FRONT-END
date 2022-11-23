f314afeb86ea44680841df4e413a7c6ec182b795
Ploy <117616170+yaminP@users.noreply.github.com>
2022-11-23 13:50:08 +0700

    editpor

--- container/Product/ProductShowDetails.jsx
+++ container/Product/ProductShowDetails.jsx
@@ -8,7 +8,12 @@ export default function ProductShowDetails() {
            
            <h1>Product Details</h1> 
             <Container>
-                
+                <img src={'images/product/marijuana_01.png'} className='img-detail' alt="imgProduct" />
+
+                <div className="products-detail">
+                    <h2>ชื่อสินค้า</h2>
+                    <h5>รายละเอียด</h5>
+                </div>
             </Container>
 
         
