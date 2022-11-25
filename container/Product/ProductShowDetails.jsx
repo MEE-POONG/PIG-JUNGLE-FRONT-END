d9877763f08b23b8e37dc7acbb3fffdf84a423c5
Ploy <117616170+yaminP@users.noreply.github.com>
2022-11-25 13:27:25 +0700

    oop

--- container/Product/ProductShowDetails.jsx
+++ container/Product/ProductShowDetails.jsx
@@ -26,7 +26,7 @@ export default function ProductShowDetails() {
                         <div className="shipping-rates">
                             <span> <FaShippingFast className="shipping-icon"/>Standard Delivery</span>
                         </div>
-                        <ul className='p-amount'>
+                        <ul className='p-count'>
                                 <li>จำนวน</li>
                                 <span><li className="bg" ><FaPlus /></li> </span>
                                 <span><li>.....</li> </span>
@@ -37,11 +37,10 @@ export default function ProductShowDetails() {
                     </div>
                 </div>
             </div>
-            <Col xs={9} md={12}>
             <Container>
                 <div className="add-on">
                     <h2>สินค้าแนะนำ</h2>
-                <Row>
+                    <Row>
                         <Col xs={12} sm={6} md={4} lg={3}>
                             <Card>
                                 <Card.Header bsPrefix='card-header'>
@@ -79,50 +78,12 @@ export default function ProductShowDetails() {
                                 </Card.Footer> 
                             </Card>
                         </Col>
-                        {/* <Col xs={12} sm={6} md={6} lg={4}>
-                            <Card>
-                                <Card.Header bsPrefix='card-header'>
-                                    <h3>ชื่อ</h3>
-                                </Card.Header>
-                                <Card.Img src={'images/product/marijuana_01.png'} className='img-product' alt="imgProduct" />
-                                <Card.Body>
-                                    <div className="details">
-                                        <h5>detail</h5>
-                                        <h4><p> price บาท</p></h4>
-                                    </div>
-                                    <ul className='p-0'>
-                                        <li>จำนวน</li>
 
-                                        <li className="bg" >
-                                            <FaPlus /></li>
-
-                                        <li>.....</li>
-
-                                        <li className="bg">
-                                            <FaMinus /></li>
-
-                                        <li>กรัม</li>
-                                    </ul>
-                                        <ul className='p-0'>
-                                            <li>
-                                                <button className='btn btn-success'>
-                                                    <FaShoppingCart className='icon' /> เพิ่มเข้าตระกร้า
-                                                </button>
-                                            </li>
-                                        </ul>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <span className="foot"><FaShoppingBag className='icon' /> ซื้อเลย</span>
-                                </Card.Footer> 
-                            </Card>
-                        </Col> */}
+                        
                     </Row>
                 </div>
-                </Container>   
-                
-            </Col>
-                
-           
+            </Container>   
+        
         </div>
     )
 }
